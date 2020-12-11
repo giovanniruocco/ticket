5da00cc2428ca7cd3e45cb9b9a0ad5b0e4a3aaaf
giovanniruocco <gionni0797@gmail.com>
2020-12-11 21:16:27 +0100

    MyTickets Activity added

--- app/src/main/java/com/ticket/app/MyTicketsActivity.java
+++ app/src/main/java/com/ticket/app/MyTicketsActivity.java
@@ -1,14 +1,190 @@
 package com.ticket.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.Context.VIBRATOR_SERVICE;
+import static java.util.Collections.reverse;
 
 public class MyTicketsActivity extends AppCompatActivity {
 
-    @Override
+    private FloatingActionButton addticket;
+    private FirebaseAuth auth;
+    private TextView welcome;
+    private Vibrator myVib;
+    private RecyclerView myrv;
+    private List<Ticket> listatickets;
+    private RecyclerViewMyTickets myAdapter;
+    private DatabaseReference myRef,myRef2;
+    private String utente,email;
+    private ImageView immagine;
+    private DatabaseReference mDatabase;
+    GoogleSignInClient mGoogleSignInClient;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_tickets);
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mDatabase=FirebaseDatabase.getInstance().getReference();
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(MyTicketsActivity.this);
+        if (acct != null && mDatabase.child("Users").child(acct.getId()).child("email").toString() != acct.getEmail())  {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+
+            FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
+            mDatabase.child("Users").child(personId).child("email").setValue(personEmail);
+            mDatabase.child("Users").child(personId).child("name").setValue(personGivenName);
+            mDatabase.child("Users").child(personId).child("surname").setValue(personFamilyName);
+        }
+
+
+
+
+
+        welcome=(TextView)findViewById(R.id.mytickets_welcome);
+        immagine=(ImageView)findViewById(R.id.mytickets_image);
+        immagine.setVisibility(View.INVISIBLE);
+        setTitle("My Tickets");
+        auth = FirebaseAuth.getInstance();
+        final Intent loginact = new Intent(MyTicketsActivity.this,LoginActivity.class);
+        if(auth.getCurrentUser() != null) {
+            utente = auth.getCurrentUser().getUid();
+            email=auth.getCurrentUser().getEmail();
+        }
+        else startActivity(loginact);
+        addticket=(FloatingActionButton)findViewById(R.id.mytickets_add);
+        myVib=(Vibrator) this.getSystemService(VIBRATOR_SERVICE);
+        listatickets = new ArrayList<>();
+        myRef= FirebaseDatabase.getInstance().getReference("Tickets");
+        myRef2=FirebaseDatabase.getInstance().getReference("Users");
+        final Query query=myRef.orderByChild("utente").equalTo(utente);
+        query.addListenerForSingleValueEvent(valueEventListener);
+        Query profilo=myRef2.orderByChild("email").equalTo(email);
+        profilo.addListenerForSingleValueEvent(evento);
+        myrv = (RecyclerView) findViewById(R.id.mytickets_recyclerview);
+        myrv.setLayoutManager(new GridLayoutManager(this, 1));
+        final Intent intentmain = new Intent(this, MainActivity.class);
+        addticket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (auth.getCurrentUser()!=null)
+                    startActivity(new Intent(MyTicketsActivity.this, AddActivity.class));
+                else {
+                    startActivity(new Intent(MyTicketsActivity.this, LoginActivity.class));
+                    Toast.makeText(MyTicketsActivity.this, "You must be logged in", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipemytickets);
+        swipeRefreshLayout.setColorSchemeResources(R.color.prova,R.color.Rosso,R.color.verde);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                query.addListenerForSingleValueEvent(valueEventListener);
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
     }
+
+    ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            listatickets.clear();
+            if (dataSnapshot.exists()) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Ticket ticket = snapshot.getValue(Ticket.class);
+                    listatickets.add(ticket);
+                }
+                reverse(listatickets);
+                myAdapter=new RecyclerViewMyTickets( MyTicketsActivity.this,listatickets);
+                myrv.setAdapter(myAdapter);
+            }
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    };
+
+    ValueEventListener evento = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            for (DataSnapshot snap: dataSnapshot.getChildren())
+            {
+                User user=snap.getValue(User.class);
+                if (listatickets.isEmpty())
+                {
+                    welcome.setText("Welcome, " + user.getName() + " " + user.getSurname() +"\nAdd your tickets.");
+                    immagine.setVisibility(View.VISIBLE);
+                }
+                else
+                    welcome.setText("Welcome, " + user.getName() + " " + user.getSurname() +"\nThese are your tickets.");
+            }
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+        }
+    };
+
+
+
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(MyTicketsActivity.this,MainActivity.class));
+    }
+
+
 }
---
+++ app/src/main/java/com/ticket/app/RecyclerViewMyTickets.java
@@ -0,0 +1,190 @@
+package com.ticket.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import static android.content.Context.VIBRATOR_SERVICE;
+
+public class RecyclerViewMyTickets extends RecyclerView.Adapter<RecyclerViewMyTickets.MyViewHolder> {
+    private Context mContext ;
+    private List<Ticket> mData ;
+    private String uid;
+    private Vibrator myVib;
+    private DatabaseReference myRef;
+    private String Name,Category,Description,Price,Date,Tel,Email,Region,City,Thumbnail,Utente;
+    private FirebaseAuth auth;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
+
+    public RecyclerViewMyTickets(Context mContext, List<Ticket> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view ;
+        LayoutInflater mInflater = LayoutInflater.from(mContext);
+        view = mInflater.inflate(R.layout.cardview_my_tickets,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        myRef= FirebaseDatabase.getInstance().getReference("Tickets");
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        Ticket currentticket= mData.get(position);
+        holder.tv_ticket_name.setText(mData.get(position).getName());
+        holder.tv_ticket_category.setText(mData.get(position).getCategory());
+
+        uid=mData.get(position).getUid();
+        auth=FirebaseAuth.getInstance();
+        if(auth.getCurrentUser() != null) {
+            Utente = auth.getCurrentUser().getUid();
+        }
+
+        holder.tv_ticket_price.setText(mData.get(position).getPrice() + " €");
+        Picasso.get()
+                .load(currentticket.getThumbnail())
+                //.placeholder(R.drawable.caricacuore)
+                .fit()
+                .centerCrop()
+                .into(holder.img_ticket_thumbnail);
+        Name=mData.get(position).getName();
+        Category=mData.get(position).getCategory();
+        Description=mData.get(position).getDescription();
+        Region=mData.get(position).getRegion();
+        City=mData.get(position).getCity();
+        Price=mData.get(position).getPrice();
+        Date=mData.get(position).getDate();
+        Tel=mData.get(position).getTel();
+        Email=mData.get(position).getEmail();
+        Thumbnail=mData.get(position).getThumbnail();
+       /*
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, ProfileDogActivity.class);
+                // passing data to the dog activity
+                intent.putExtra("Name",mData.get(position).getName());
+                intent.putExtra("Breed",mData.get(position).getCategory());
+                intent.putExtra("Description",mData.get(position).getDescription());
+                intent.putExtra("Region",mData.get(position).getRegion());
+                intent.putExtra("City",mData.get(position).getCity());
+                intent.putExtra("Price",mData.get(position).getPrice());
+                intent.putExtra("Date",mData.get(position).getDate());
+                intent.putExtra("Tel",mData.get(position).getTel());
+                intent.putExtra("Email",mData.get(position).getEmail());
+                intent.putExtra("Thumbnail",mData.get(position).getThumbnail());
+                intent.putExtra("Uid",mData.get(position).getUid());
+                mContext.startActivity(intent);
+            }
+        });
+*/
+
+       /*
+        holder.cardView.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View v) {
+                final CharSequence[] items={"Edit dog", "Delete dog","Back"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setTitle("What do you want to do?");
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (items[i].equals("Edit dog")) {
+                            Intent intento = new Intent(mContext,EditActivity.class);
+                            intento.putExtra("Name",mData.get(position).getName());
+                            intento.putExtra("Breed",mData.get(position).getBreed());
+                            intento.putExtra("Description",mData.get(position).getDescription());
+                            intento.putExtra("Gender",mData.get(position).getGender());
+                            intento.putExtra("City",mData.get(position).getCity());
+                            intento.putExtra("Age",mData.get(position).getAge());
+                            intento.putExtra("Tel",mData.get(position).getTel());
+                            intento.putExtra("Email",mData.get(position).getEmail());
+                            intento.putExtra("Image",mData.get(position).getThumbnail());
+                            intento.putExtra("Uid",mData.get(position).getUid());
+                            mContext.startActivity(intento);
+                            dialogInterface.dismiss();
+                        }else if (items[i].equals("Back")) {
+                            dialogInterface.dismiss();
+                        }
+                        else if (items[i].equals("Delete dog"))
+                        {
+                            dialogInterface.dismiss();
+
+                            new AlertDialog.Builder(mContext)
+                                    .setMessage("Are you sure you want to delete " + mData.get(position).getName() +"?")
+                                    .setCancelable(true)
+                                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            myRef.child(mData.get(position).getUid()).setValue(null);
+                                            StorageReference cancella = storage.getReferenceFromUrl(mData.get(position).getThumbnail());
+                                            cancella.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                }
+                                            });
+                                            mContext.startActivity(new Intent(mContext,ProfileActivity.class));
+                                        }
+                                    })
+                                    .setNegativeButton("No", null)
+                                    .show();
+                        }
+                    }
+                });
+                builder.show();
+                myVib=(Vibrator) mContext.getSystemService(VIBRATOR_SERVICE);
+                return true;
+            }
+        });
+
+        */
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView tv_ticket_name;
+        TextView tv_ticket_category;
+        TextView tv_ticket_price;
+        ImageView img_ticket_thumbnail;
+        CardView cardView ;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            tv_ticket_name = (TextView) itemView.findViewById(R.id.ticket_name_id_profile) ;
+            tv_ticket_category=(TextView) itemView.findViewById(R.id.ticket_category_id_profile);
+            tv_ticket_price=(TextView)itemView.findViewById(R.id.ticket_price_profile);
+            img_ticket_thumbnail = (ImageView) itemView.findViewById(R.id.ticket_img_id_profile);
+            cardView = (CardView) itemView.findViewById(R.id.cardview_id_profile);
+        }
+    }
+}
