986b1a2c56d4a10473dd33b2851db0794f61c9fc
giovanniruocco <gionni0797@gmail.com>
2021-01-18 11:37:13 +0100

    DeveloperActivity

--- app/src/main/java/com/ticket/app/DeveloperActivity.java
+++ app/src/main/java/com/ticket/app/DeveloperActivity.java
@@ -12,50 +12,13 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class DeveloperActivity extends AppCompatActivity {;
-    private EditText et_date;
-    private Calendar myCalendar;
-    private DatePickerDialog.OnDateSetListener date;
+public class DeveloperActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_developer);
 
-        myCalendar = Calendar.getInstance();
 
-        et_date= (EditText) findViewById(R.id.Birthday);
-        date = new DatePickerDialog.OnDateSetListener() {
-
-            @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear,
-                                  int dayOfMonth) {
-                // TODO Auto-generated method stub
-                myCalendar.set(Calendar.YEAR, year);
-                myCalendar.set(Calendar.MONTH, monthOfYear);
-                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateLabel();
-            }
-
-        };
-
-        et_date.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                new DatePickerDialog(DeveloperActivity.this, date, myCalendar
-                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-            }
-        });
-
-    }
-
-    private void updateLabel() {
-        String myFormat = "MM/dd/yy"; //In which you need put here
-        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-
-        et_date.setText(sdf.format(myCalendar.getTime()));
     }
 }
