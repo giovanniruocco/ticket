4d0392633bb04eab2eef6fe5fa3a27d50c7637fe
giovanniruocco <gionni0797@gmail.com>
2021-01-18 13:59:30 +0100

    DeveloperActivity

--- app/src/main/java/com/ticket/app/DeveloperActivity.java
+++ app/src/main/java/com/ticket/app/DeveloperActivity.java
@@ -18,6 +18,7 @@ public class DeveloperActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_developer);
+        setTitle("Developers");
 
 
     }
